197fbc4be1ce2d5b60a0ed534d13ea17e98aa4d9
Wanis Chouaib <wanis.chouaib@ens.uvsq.fr>
2022-04-30 23:08:22 +0200

    Sauvegarde W

--- Projet_jeu_2048.py
+++ Projet_jeu_2048.py
@@ -14,43 +14,44 @@ racine.title("2048")
 
     # Variables globales
         
-cells = []   # contient chaque détail de chaque tuile (couleur+nombre)
+cellules = []   # contient chaque détail de chaque tuile (couleur+nombre)
 matrice = [] # contient les nombres présents sur la grille
 end = None   # cette variable donnera si la partie est gagnée ou perdue,
               # 1 = gagné et 0 = perdu
 
 
-    # Fonctions permettant de jouer  
+    # Fonctions premières  
        
 def Creation_Interface():
-    # création de la plateforme de jeu
-    global cells
+    """Cette fonction va créer la plateforme de jeu"""
+    global cellules
     for i in range (4):
-        row = []
+        ligne = []
         for j in range (4):
-            cell_frame = tk.Frame(background, bg=c.EMPTY_CELL_COLOR, width=120, height=120)
-            cell_frame.grid(row=i, column=j, padx=5, pady=5)
-            cell_number = tk.Label(background, bg=c.EMPTY_CELL_COLOR)
-            cell_number.grid(row=i, column=j)
-            cell_data = {"frame": cell_frame, "number": cell_number}
-            row.append(cell_data)
-        cells.append(row)
+            cellule_frame = tk.Frame(background, bg=c.EMPTY_CELL_COLOR, width=120, height=120)
+            cellule_frame.grid(row=i, column=j, padx=5, pady=5)
+            cellule_number = tk.Label(background, bg=c.EMPTY_CELL_COLOR)
+            cellule_number.grid(row=i, column=j)
+            cellule_data = {"frame": cellule_frame, "number": cellule_number}
+            ligne.append(cellule_data)
+        cellules.append(ligne)
     
     
-def Actualisation_Interface(mat):
+def Actualisation_Interface():
     """Cette fonction va actualiser les couleurs/affichages dans le plateau"""
+    global cellules
     for i in range (4):
         for j in range (4):
-            valeur_cell = mat[i][j]
-            if valeur_cell == 0:
-                cells[i][j]["frame"].config(bg=c.EMPTY_CELL_COLOR)
-                cells[i][j]["number"].config(bg=c.EMPTY_CELL_COLOR, text="") 
+            valeur_cellule = matrice[i][j]
+            if valeur_cellule == 0:
+                cellules[i][j]["frame"].config(bg=c.EMPTY_CELL_COLOR)
+                cellules[i][j]["number"].config(bg=c.EMPTY_CELL_COLOR, text="") 
             else:
-                cells[i][j]["frame"].config(bg=c.CELL_COLORS[valeur_cell])
-                cells[i][j]["number"].config(bg=c.CELL_COLORS[valeur_cell], 
-                                             fg=c.CELL_NUMBER_COLORS[valeur_cell],
-                                             font=c.CELL_NUMBER_FONTS[valeur_cell],
-                                             text=str(valeur_cell))
+                cellules[i][j]["frame"].config(bg=c.CELL_COLORS[valeur_cellule])
+                cellules[i][j]["number"].config(bg=c.CELL_COLORS[valeur_cellule], 
+                                             fg=c.CELL_NUMBER_COLORS[valeur_cellule],
+                                             font=c.CELL_NUMBER_FONTS[valeur_cellule],
+                                             text=str(valeur_cellule))
     
 
 def Generateur_Tuile(mat):
@@ -61,9 +62,11 @@ def Generateur_Tuile(mat):
         row = rd.randint(0,3)
         col = rd.randint(0,3)
     mat[row][col] = np.random.choice(np.arange(2, 5, 2), p=[0.9, 0.1])
+    return mat
 
 
-def Empiler(mat):
+    # Empilation
+def Empiler_Gauche(mat):
     """Place les tuiles vers la gauche"""
     matrice2 = [[0]*4 for _ in range (4)]
     for i in range (4):
@@ -72,16 +75,81 @@ def Empiler(mat):
             if mat[i][j] != 0:
                 matrice2[i][pos] = mat[i][j]
                 pos+=1
-    mat = matrice2
+    mat = matrice2  
+    return mat
     
+def Empiler_Droite(mat):
+    """Place les tuiles vers la droite"""
+    matrice2 = [[0]*4 for _ in range (4)]
+    for i in range (4):
+        pos = -1
+        for j in range (4):
+            if mat[i][j] != 0:
+                matrice2[i][pos] = mat[i][j]
+                pos-=1
+    mat = matrice2
+    return mat
 
-def Combiner(mat):
-    """ Cette fonction permettra de combiner 2 tuiles de même nombre"""
+def Empiler_Haut(mat):
+    """Place les tuiles vers le haut"""
+    matrice2 = [[0]*4 for _ in range (4)]
+    for j in range (4):
+        pos = 0
+        for i in range (4):
+            if mat[i][j] != 0:
+                matrice2[pos][j] = mat[i][j]
+                pos+=1
+    mat = matrice2
+    return mat
+
+def Empiler_Bas(mat):
+    """Place les tuiles vers le bas"""
+    matrice2 = [[0]*4 for _ in range (4)]
+    for j in range (4):
+        pos = -1
+        for i in range (4):
+            if mat[i][j] != 0:
+                matrice2[pos][j] = mat[i][j]
+                pos-=1
+    mat = matrice2
+    return mat
+    
+    # Combinaison
+def Combiner_Gauche(mat):
+    """ Cette fonction permettra de combiner 2 tuiles de même nombre vers la gauche"""
     for i in range (4):
         for j in range (3):
             if mat[i][j] != 0 and mat[i][j] == mat[i][j+1]:
                 mat[i][j] *= 2
                 mat[i][j+1] = 0
+    return mat
+                
+def Combiner_Droite(mat):
+    """ Cette fonction permettra de combiner 2 tuiles de même nombre vers la droite"""
+    for i in range (4):
+        for j in range (1,4):
+            if mat[i][-j] != 0 and mat[i][-j] == mat[i][-j-1]:
+                mat[i][-j] *= 2
+                mat[i][-j-1] = 0
+    return mat
+                
+def Combiner_Haut(mat):
+    """ Cette fonction permettra de combiner 2 tuiles de même nombre vers le haut"""
+    for i in range (3):
+        for j in range (4):
+            if mat[i][j] != 0 and mat[i][j] == mat[i+1][j]:
+                mat[i][j] *= 2
+                mat[i+1][j] = 0
+    return mat
+                
+def Combiner_Bas(mat):
+    """ Cette fonction permettra de combiner 2 tuiles de même nombre vers le bas"""
+    for i in range (1,4):
+        for j in range (4):
+            if mat[-i][j] != 0 and mat[-i][j] == mat[-i-1][j]:
+                mat[-i][j] *= 2
+                mat[-i-1][j] = 0
+    return mat
                 
 
            
@@ -107,11 +175,11 @@ def Transposer(mat):
        
 def Generateur():
     """ Cette fonction génère 2 tuiles aléatoirement et les affiche dans le jeu """
-    global cells
+    global cellules
     global matrice
     # réinitialiser la plateforme de jeu ainsi que les 2 tuiles placées
     matrice = []
-    cells = []
+    cellules = []
     Creation_Interface()
     
     # début du code
@@ -122,8 +190,8 @@ def Generateur():
     col = rd.randint(0,3)
     tuile = np.random.choice(np.arange(2, 5, 2), p=[0.9, 0.1])
     matrice[row][col] = tuile 
-    cells[row][col]["frame"].config(bg=c.CELL_COLORS[tuile])
-    cells[row][col]["number"].config(bg=c.CELL_COLORS[tuile], 
+    cellules[row][col]["frame"].config(bg=c.CELL_COLORS[tuile])
+    cellules[row][col]["number"].config(bg=c.CELL_COLORS[tuile], 
                                      fg=c.CELL_NUMBER_COLORS[tuile], 
                                      font=c.CELL_NUMBER_FONTS[tuile], 
                                      text=str(tuile)
@@ -133,8 +201,8 @@ def Generateur():
         col = rd.randint(0,3)
     tuile = np.random.choice(np.arange(2, 5, 2), p=[0.9, 0.1])
     matrice[row][col] = tuile 
-    cells[row][col]["frame"].config(bg=c.CELL_COLORS[tuile])
-    cells[row][col]["number"].config(bg=c.CELL_COLORS[tuile], 
+    cellules[row][col]["frame"].config(bg=c.CELL_COLORS[tuile])
+    cellules[row][col]["number"].config(bg=c.CELL_COLORS[tuile], 
                                      fg=c.CELL_NUMBER_COLORS[tuile], 
                                      font=c.CELL_NUMBER_FONTS[tuile], 
                                      text=str(tuile)
@@ -171,63 +239,48 @@ def Load_Button():
     
     matrice = matrice2
 
-    Actualisation_Interface(matrice)
+    Actualisation_Interface()
     
-
 
     # Fonctions associées aux déplacements
 
 def Left_Button():
-    try:
-        Empiler(matrice)
-        Combiner(matrice)
-        Empiler(matrice)
-        Generateur_Tuile(matrice)
-        Actualisation_Interface(matrice)
-        Game_Over()
-    except:
-        pass
+    global matrice
+    matrice = Empiler_Gauche(matrice)
+    matrice = Combiner_Gauche(matrice)
+    matrice = Empiler_Gauche(matrice)
+    matrice = Generateur_Tuile(matrice)
+    Actualisation_Interface()
+    #Game_Over()
+
 
 def Right_Button():
-    try:
-        Inverser(matrice)
-        Empiler(matrice)
-        Combiner(matrice)
-        Empiler(matrice)
-        Inverser(matrice)
-        Generateur_Tuile(matrice)
-        Actualisation_Interface(matrice)
-        Game_Over()
-    except:
-        pass
+    global matrice
+    matrice = Empiler_Droite(matrice)
+    matrice = Combiner_Droite(matrice)
+    matrice = Empiler_Droite(matrice)
+    matrice = Generateur_Tuile(matrice)
+    Actualisation_Interface()
+    #Game_Over()
 
 def Up_Button():
-    try:
-        Transposer(matrice)
-        Empiler(matrice)
-        Combiner(matrice)
-        Empiler(matrice)
-        Transposer(matrice)
-        Generateur_Tuile(matrice)
-        Actualisation_Interface(matrice)
-        Game_Over()
-    except:
-        pass
+    global matrice
+    matrice = Empiler_Haut(matrice)
+    matrice = Combiner_Haut(matrice)
+    matrice = Empiler_Haut(matrice)
+    matrice = Generateur_Tuile(matrice)
+    Actualisation_Interface()
+    #Game_Over()
 
 def Down_Button():
-    try:
-        Transposer(matrice)
-        Inverser(matrice)
-        Empiler(matrice)
-        Combiner(matrice)
-        Empiler(matrice)
-        Inverser(matrice)
-        Generateur_Tuile(matrice)
-        Actualisation_Interface(matrice)
-        Game_Over()         
-    except:
-        pass
-   
+    global matrice
+    matrice = Empiler_Bas(matrice)
+    matrice = Combiner_Bas(matrice)
+    matrice = Empiler_Bas(matrice)
+    matrice = Generateur_Tuile(matrice)
+    Actualisation_Interface()
+    #Game_Over()
+         
     
     # Fonctions associées aux tests au cours du jeu
 
@@ -274,9 +327,7 @@ def Affich_Game_Over(): #//créer un fond tout blanc pour afficher winner ou loo
                  fg=c.GAME_OVER_FONT_COLOR,
                  font=c.GAME_OVER_FONT).pack()
      
-    
       
-    
     # Boutons fonctionnement   
     
 Start = tk.Button(text="Start", 
